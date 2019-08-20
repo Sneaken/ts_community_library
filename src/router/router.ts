964a69bff12082f4bfbdfd7bafe18f3870a6ed08
Sneanken <924393527@qq.com>
2019-08-20 23:41:03 +0800

    初步创建项目

---
+++ src/router/router.ts
@@ -0,0 +1,58 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Home from '../views/Home.vue'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (about.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () =>
+//         import(/* webpackChunkName: "about" */ '../views/About.vue')
+//     }
+//   ]
+// })
+/**
+ * meta 可配置参数
+ * @param {boolean} icon 页面icon
+ * @param {boolean} keepAlive 是否缓存页面
+ * @param {string} title 页面标题
+ */
+export default [
+  {
+    path: '/',
+    redirect: '/index'
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/login.vue'),
+    meta: {
+      icon: '',
+      keepAlive: true,
+      title: 'login'
+    }
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: () => import('@/views/index/index.vue'),
+    meta: {
+      icon: '',
+      keepAlive: true,
+      title: 'index'
+    }
+  }
+]
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template> </template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Login extends Vue {
+  // initial data
+  msg = 123
+
+  // use prop values for initial data
+  helloMsg = 'Hello, ' + this.propMessage
+
+  // lifecycle hook
+  mounted() {
+    this.greet()
+  }
+
+  // computed
+  get computedMsg() {
+    return 'computed ' + this.msg
+  }
+
+  // method
+  greet() {
+    alert('greeting: ' + this.msg)
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
